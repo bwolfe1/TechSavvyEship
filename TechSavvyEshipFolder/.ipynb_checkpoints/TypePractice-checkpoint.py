6bfe1305709ef04bbebf3aa7aed3d6fc814ac99a
bwolfe1 <bwolfe1@babson.edu>
2019-03-16 15:11:20 -0400

    almost done saturday

--- TechSavvyEshipFolder/.ipynb_checkpoints/TypePractice-checkpoint.py
+++ TechSavvyEshipFolder/.ipynb_checkpoints/TypePractice-checkpoint.py
@@ -1,4 +1,40 @@
-print(3.14e-2)
+##Numbers
 
-length_of_number= len(str(2**1000))
-print(length_of_number)
+#print(3.14e-2)
+
+#length_of_number= len(str(2**1000))
+#print(length_of_number)
+
+import random
+
+#print(random.randint(1,72))
+#print(random.choice(["Bailey", "Joy", "Lala", "Erika"]))
+
+## Letters/strings
+
+#print("I'm saying \"OK\"\n")
+#print("I am learning: \n\nPython")
+
+## Boolean
+
+#print(random.randint(1,3)==2)
+#print (3>2 and 3>5) #False (and means if EITHER is false FALSE)
+#print(3>2 or 3>5) # True (or means if ANY is true TRUE)
+
+age=int(input("type your age as an integer"))
+
+if age >=21: 
+    print("yes you can")
+else:
+    country=int(input("type 1 if you live in USA, otherwise type 0"))
+    if country==0:
+        print("yes you can")
+        
+    else: print("you may not")
+
+## in a boolean float and integer = eachother if VALUE is the same
+## in boolean equation with othernumbers Boolean True=1 False=0
+## EX: (1!=0)-1 = True -1 = 1-1 = 0
+
+
+    
